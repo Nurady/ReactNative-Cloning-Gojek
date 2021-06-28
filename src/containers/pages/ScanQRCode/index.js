93f3a7e8823a43da0772f0a0659a168cf0090a29
unknown <nursyafriady83@gmail.com>
2021-06-28 19:39:30 +0800

    add React Native Camera

--- src/containers/pages/ScanQRCode/index.js
+++ src/containers/pages/ScanQRCode/index.js
@@ -1,25 +1,69 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {Gap} from '../../../components';
+import {RNCamera} from 'react-native-camera';
 
-const ScanQRCode = () => {
-  return (
-    <View style={styles.page}>
-      <View style={styles.content} />
-      <View style={styles.optionWrapper}>
-        <View style={styles.lineWrapper}>
-          <View style={styles.line} />
-          <Gap height={3} />
-          <View style={styles.line} />
+class ScanQRCode extends Component {
+  takePicture = async () => {
+    if (this.camera) {
+      const options = {quality: 0.5, base64: true};
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.page}>
+        <View style={styles.content} />
+        <View style={styles.container}>
+          <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            androidCameraPermissionOptions={{
+              title: 'Permission to use camera',
+              message: 'We need your permission to use your camera',
+              buttonPositive: 'Ok',
+              buttonNegative: 'Cancel',
+            }}
+            androidRecordAudioPermissionOptions={{
+              title: 'Permission to use audio recording',
+              message: 'We need your permission to use your audio',
+              buttonPositive: 'Ok',
+              buttonNegative: 'Cancel',
+            }}
+            onGoogleVisionBarcodesDetected={({barcodes}) => {
+              console.log(barcodes);
+            }}
+          />
+          <View
+            style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+            <TouchableOpacity
+              onPress={this.takePicture.bind(this)}
+              style={styles.capture}>
+              <Text style={{fontSize: 14}}> SNAP </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.optionContent}>
-          <Text style={styles.text}>More Options</Text>
-          <Text style={styles.text}>Shortcut</Text>
+        <View style={styles.optionWrapper}>
+          <View style={styles.lineWrapper}>
+            <View style={styles.line} />
+            <Gap height={3} />
+            <View style={styles.line} />
+          </View>
+          <View style={styles.optionContent}>
+            <Text style={styles.text}>More Options</Text>
+            <Text style={styles.text}>Shortcut</Text>
+          </View>
         </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 export default ScanQRCode;
 
@@ -53,5 +97,24 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 18,
     fontWeight: '600',
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
   },
 });
